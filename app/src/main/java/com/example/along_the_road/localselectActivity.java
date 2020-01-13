519cf07b8648fe15b51bad5b572840e5b6a9475a
Kimnaram <snrneh3@naver.com>
2020-01-13 18:42:02 +0900

    화면 전환하기 수정 필요, 메인 화면 버튼_200113

--- app/src/main/java/com/example/along_the_road/localselectActivity.java
+++ app/src/main/java/com/example/along_the_road/localselectActivity.java
@@ -9,27 +9,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button intent_lsbtn;
+public class localselectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_local);
         Log.d("ACTIVITY_LC", "onCreate 호출됨");
         Toast.makeText(getApplicationContext(), "onCreate 호출됨", Toast.LENGTH_SHORT).show();
 
-        intent_lsbtn = findViewById(R.id.localselectbutton);
-
-        intent_lsbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onLocalSelectClick(View view) {
-                Intent intent = new Intent(MainActivity.this, localselectActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
-
-
