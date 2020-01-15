af3bdc50440b4d61dc16249dc4f443af32cd4af7
Kimnaram <snrneh3@naver.com>
2020-01-15 23:13:16 +0900

    날짜 선택하기_버튼으로 구현_200115

--- app/src/main/java/com/example/along_the_road/dayselectActivity.java
+++ app/src/main/java/com/example/along_the_road/dayselectActivity.java
@@ -2,22 +2,50 @@ package com.example.along_the_road;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class dayselectActivity extends AppCompatActivity {
 
-    @Override
+    private TextView textView_Date;
+    private DatePickerDialog.OnDateSetListener callbackMethod;
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dayselect);
 
-        TextView tx1 = (TextView)findViewById(R.id.textView);
+        this.InitiallizeView();
+        this.InitializeListener();
+
+        TextView tx1 = (TextView)findViewById(R.id.city);
 
         Intent intent = getIntent();
 
         String city = intent.getExtras().getString("city");
         tx1.setText(city);
+    }
+
+    public void InitiallizeView() {
+       textView_Date = (TextView)findViewById(R.id.dayselect);
+    }
+
+    public void InitializeListener() {
+       callbackMethod = new DatePickerDialog.OnDateSetListener() {
+           @Override
+           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+               textView_Date.setText(year + "년" + month + "월" + dayOfMonth + "일");
+           }
+       };
+    }
+
+    public void OnclickHandler(View view) {
+       DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 5, 24);
+
+       dialog.show();
     }
 }
