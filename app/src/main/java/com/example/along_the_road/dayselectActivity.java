05b1c2ee381f6c1a43fe906797de99bf2d75a6b3
Kimnaram <snrneh3@naver.com>
2020-01-21 00:04:42 +0900

    날짜 선택하기_출발/도착 선택_수정본_200120

--- app/src/main/java/com/example/along_the_road/dayselectActivity.java
+++ app/src/main/java/com/example/along_the_road/dayselectActivity.java
@@ -6,13 +6,16 @@ import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
 public class dayselectActivity extends AppCompatActivity {
 
-    private TextView textView_Date;
-    private DatePickerDialog.OnDateSetListener callbackMethod;
+    private DatePickerDialog.OnDateSetListener acallbackMethod;
+    private DatePickerDialog.OnDateSetListener scallbackMethod;
+    private Button start_date;
+    private Button arrive_date;
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,32 +23,48 @@ public class dayselectActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dayselect);
 
         this.InitiallizeView();
-        this.InitializeListener();
+        this.InitializesListener();
+        this.InitializeaListener();
 
-        TextView tx1 = (TextView)findViewById(R.id.city);
+//        TextView tx1 = (TextView)findViewById(R.id.city);
 
-        Intent intent = getIntent();
-
-        String city = intent.getExtras().getString("city");
-        tx1.setText(city);
+//        Intent intent = getIntent();
+//        String city = intent.getExtras().getString("city");
+//        tx1.setText(city);
     }
 
     public void InitiallizeView() {
-       textView_Date = (TextView)findViewById(R.id.dayselect);
+       start_date = (Button) findViewById(R.id.startdate_btn);
+       arrive_date = (Button) findViewById(R.id.arrivedate_btn);
     }
 
-    public void InitializeListener() {
-       callbackMethod = new DatePickerDialog.OnDateSetListener() {
+    public void InitializesListener() {
+       scallbackMethod = new DatePickerDialog.OnDateSetListener() {
            @Override
            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-               textView_Date.setText(year + "년" + month + "월" + dayOfMonth + "일을 선택하셨습니다.");
+               start_date.setText(year + "년" + month + "월" + dayOfMonth + "일을 선택하셨습니다.");
            }
        };
     }
 
-    public void OnclickHandler(View view) {
-       DatePickerDialog dialog = new DatePickerDialog(this, callbackMethod, 2019, 5, 24);
+    public void InitializeaListener() {
+       acallbackMethod = new DatePickerDialog.OnDateSetListener() {
+           @Override
+           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+               arrive_date.setText(year + "년" + month + "월" + dayOfMonth + "일을 선택하셨습니다.");
+           }
+       };
+    }
+
+    public void OnaclickHandler(View view) {
+       DatePickerDialog dialog = new DatePickerDialog(this, acallbackMethod, 2020, 1, 20);
 
        dialog.show();
+    }
+
+    public void OnsclickHandler(View view) {
+        DatePickerDialog dialog = new DatePickerDialog(this, scallbackMethod, 2020, 1, 20);
+
+        dialog.show();
     }
 }
