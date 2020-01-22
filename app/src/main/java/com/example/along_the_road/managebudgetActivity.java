e366385f1750776dcdeb38493d31f951723830c3
Kimnaram <snrneh3@naver.com>
2020-01-22 15:46:04 +0900

    지출 등록하기 수정본_200122

--- app/src/main/java/com/example/along_the_road/managebudgetActivity.java
+++ app/src/main/java/com/example/along_the_road/managebudgetActivity.java
@@ -18,12 +18,26 @@ public class managebudgetActivity extends AppCompatActivity {
         final EditText input = (EditText) findViewById(R.id.input);
         Button budget = (Button) findViewById(R.id.budget);
         final TextView output = (TextView) findViewById(R.id.output);
+        final TextView among = (TextView) findViewById(R.id.among);
+        final int count = 0;
+        final String s_among = Integer.toString(count);
+        among.setText(s_among);
 
         budget.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String add = input.getText().toString();
+                String s_cnt = among.getText().toString();
+                int cnt = count;
+                if(s_cnt != null) {
+                    cnt = Integer.parseInt(s_cnt);
+                }
+                cnt = cnt + Integer.parseInt(add);
+                String str_among = Integer.toString(cnt);
+                among.setText(str_among);
                 output.append("\n");
                 output.append(input.getText());
+
             }
         });
     }
