bd35b26623255494adf02f91a6cacf2bfcd8765d
Kimnaram <snrneh3@naver.com>
2020-01-23 22:35:39 +0900

    여행 기간 선택하기 화면 수정중_200123

--- app/src/main/java/com/example/along_the_road/dayselectActivity.java
+++ app/src/main/java/com/example/along_the_road/dayselectActivity.java
@@ -7,8 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class dayselectActivity extends AppCompatActivity {
 
@@ -26,11 +28,43 @@ public class dayselectActivity extends AppCompatActivity {
         this.InitializesListener();
         this.InitializeaListener();
 
-//        TextView tx1 = (TextView)findViewById(R.id.city);
+        Intent intent = getIntent();
+        final String city = intent.getExtras().getString("city");
+        final Button next_btn = (Button)findViewById(R.id.next);
+        final Button start_btn = (Button)findViewById(R.id.startdate_btn);
+        final Button arrive_btn = (Button)findViewById(R.id.arrivedate_btn);
 
-//        Intent intent = getIntent();
-//        String city = intent.getExtras().getString("city");
-//        tx1.setText(city);
+        final String start_date = start_btn.getText().toString();
+        final String arrive_date = arrive_btn.getText().toString();
+
+       //CalendarView 인스턴스 만들기
+       CalendarView calendar = (CalendarView)findViewById(R.id.calendar);
+
+       //리스너 등록
+       calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+           @Override
+           public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+
+               // TODO Auto-generated method stub
+               String start_day = year + "-" + month+1 + "-" + dayOfMonth;
+               Toast.makeText(dayselectActivity.this, year+"-"+(month+1)+"-"+dayOfMonth, Toast.LENGTH_SHORT).show();
+           }
+       });
+
+        next_btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Intent intent = new Intent(getApplicationContext(), hotelselectActivity.class);
+
+               intent.putExtra("city", city);
+               intent.putExtra("s_date ", start_date);
+               intent.putExtra("a_date", arrive_date);
+
+               startActivity(intent);
+           }
+       });
+
     }
 
     public void InitiallizeView() {
