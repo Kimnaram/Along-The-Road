67c38a68b2a0e798e844b92069926da804df5a62
Kimnaram <snrneh3@naver.com>
2020-01-24 22:17:12 +0900

    여행 기간 선택하기 화면 수정중_200124

--- app/src/main/java/com/example/along_the_road/dayselectActivity.java
+++ app/src/main/java/com/example/along_the_road/dayselectActivity.java
@@ -2,43 +2,43 @@ package com.example.along_the_road;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
-import android.widget.DatePicker;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class dayselectActivity extends AppCompatActivity {
 
-    private DatePickerDialog.OnDateSetListener acallbackMethod;
-    private DatePickerDialog.OnDateSetListener scallbackMethod;
-    private Button start_date;
-    private Button arrive_date;
+    private int count = 0;
+    private String start_date = "";
+    private String end_date = "";
+//    private CalendarView calendar = (CalendarView)findViewById(R.id.calendar);
 
    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dayselect);
 
-        this.InitiallizeView();
-        this.InitializesListener();
-        this.InitializeaListener();
-
         Intent intent = getIntent();
         final String city = intent.getExtras().getString("city");
         final Button next_btn = (Button)findViewById(R.id.next);
-        final Button start_btn = (Button)findViewById(R.id.startdate_btn);
-        final Button arrive_btn = (Button)findViewById(R.id.arrivedate_btn);
 
-        final String start_date = start_btn.getText().toString();
-        final String arrive_date = arrive_btn.getText().toString();
+       SimpleDateFormat format1 = new SimpleDateFormat ( "MMddyyyy");
 
-       //CalendarView 인스턴스 만들기
+       Date date = new Date();
+
+       String min_date = format1.format(date);
+       long min_date_l = Long.parseLong(min_date);
+
+       System.out.println(min_date);
+
        CalendarView calendar = (CalendarView)findViewById(R.id.calendar);
+       calendar.setMinDate(min_date_l);
 
        //리스너 등록
        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -47,58 +47,51 @@ public class dayselectActivity extends AppCompatActivity {
            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
 
                // TODO Auto-generated method stub
-               String start_day = year + "-" + month+1 + "-" + dayOfMonth;
-               Toast.makeText(dayselectActivity.this, year+"-"+(month+1)+"-"+dayOfMonth, Toast.LENGTH_SHORT).show();
+               count += 1;
+
+
+               if(count == 1) {
+                   start_date = "" +year + (month + 1) + dayOfMonth;
+                   Toast.makeText(dayselectActivity.this, "출발 날짜 : "+year + "-" + (month + 1) + "-" + dayOfMonth, Toast.LENGTH_SHORT).show();
+               }
+
+               else if(count == 2) {
+                   String temp = ""+year + (month + 1) + dayOfMonth;
+                   int s_d = Integer.parseInt(start_date);
+                   int e_d = Integer.parseInt(temp);
+                   if(s_d >= e_d) {
+                       Toast.makeText(getApplicationContext(), "도착 날짜가 출발 날짜보다 먼저일 수 없습니다.", Toast.LENGTH_SHORT).show();
+                   }
+                   else if(s_d < e_d) {
+                       end_date = ""+year + (month + 1) + dayOfMonth;
+                       Toast.makeText(dayselectActivity.this, "도착 날짜 : "+year + "-" + (month + 1) + "-" + dayOfMonth, Toast.LENGTH_SHORT).show();
+
+                       next_btn.setOnClickListener(new View.OnClickListener() {
+                           @Override
+                           public void onClick(View view) {
+                               Intent intent = new Intent(getApplicationContext(), hotelselectActivity.class);
+
+                               intent.putExtra("city", city);
+                               intent.putExtra("s_date ", start_date);
+                               intent.putExtra("a_date", end_date);
+
+                               startActivity(intent);
+                           }
+                       });
+
+                   }
+                   count = 0;
+               }
+
            }
        });
 
         next_btn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               Intent intent = new Intent(getApplicationContext(), hotelselectActivity.class);
-
-               intent.putExtra("city", city);
-               intent.putExtra("s_date ", start_date);
-               intent.putExtra("a_date", arrive_date);
-
-               startActivity(intent);
+               Toast.makeText(getApplicationContext(), "여행 기간을 선택해주세요.", Toast.LENGTH_SHORT).show();
            }
        });
 
-    }
-
-    public void InitiallizeView() {
-       start_date = (Button) findViewById(R.id.startdate_btn);
-       arrive_date = (Button) findViewById(R.id.arrivedate_btn);
-    }
-
-    public void InitializesListener() {
-       scallbackMethod = new DatePickerDialog.OnDateSetListener() {
-           @Override
-           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-               start_date.setText(year + "년" + month + "월" + dayOfMonth + "일을 선택하셨습니다.");
-           }
-       };
-    }
-
-    public void InitializeaListener() {
-       acallbackMethod = new DatePickerDialog.OnDateSetListener() {
-           @Override
-           public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-               arrive_date.setText(year + "년" + month + "월" + dayOfMonth + "일을 선택하셨습니다.");
-           }
-       };
-    }
-
-    public void OnaclickHandler(View view) {
-       DatePickerDialog dialog = new DatePickerDialog(this, acallbackMethod, 2020, 1, 20);
-
-       dialog.show();
-    }
-
-    public void OnsclickHandler(View view) {
-        DatePickerDialog dialog = new DatePickerDialog(this, scallbackMethod, 2020, 1, 20);
-
-        dialog.show();
     }
 }
