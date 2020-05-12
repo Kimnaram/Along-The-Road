c64237da964fd8b199e408e021dc668b5a7ee876
YunSongHui <dbsthdgml123@naver.com>
2020-05-12 17:51:19 +0900

    지역선택페이지 이미지뷰 테스트 + 토글버튼 크기 위치 수정 필요_20/05/12

--- app/src/main/java/com/example/along_the_road/localselectActivity.java
+++ app/src/main/java/com/example/along_the_road/localselectActivity.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.ToggleButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class localselectActivity extends AppCompatActivity {
@@ -14,77 +14,40 @@ public class localselectActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
 
-        final Button seoul_btn = (Button)findViewById(R.id.seoul_button);
-        final Button sokcho_btn = (Button)findViewById(R.id.sokcho_button);
-        final Button incheon_btn = (Button)findViewById(R.id.incheon_button);
-        final Button jeonju_btn = (Button)findViewById(R.id.jeonju_button);
-        final Button daegu_btn = (Button)findViewById(R.id.daegu_button);
-        final Button busan_btn = (Button)findViewById(R.id.busan_button);
 
-        seoul_btn.setOnClickListener(new View.OnClickListener() {
+        Button local_btn = (Button)findViewById(R.id.local_conf_btn); //다음페이지
+
+        local_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DaySelectActivity.class);
-
-                intent.putExtra("city", seoul_btn.getText());
-
-                startActivity(intent);
+                Intent local_to_day = new Intent(getApplicationContext(), DaySelectActivity.class);
+                startActivity(local_to_day);
             }
         });
 
-        sokcho_btn.setOnClickListener(new View.OnClickListener() {
+
+        final ToggleButton tb2 =
+                (ToggleButton) this.findViewById(R.id.locate_btn_busan);
+
+        tb2.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DaySelectActivity.class);
-
-                intent.putExtra("city", sokcho_btn.getText());
-
-                startActivity(intent);
-            }
-        });
-
-        incheon_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DaySelectActivity.class);
-
-                intent.putExtra("city", incheon_btn.getText());
-
-                startActivity(intent);
-            }
-        });
-
-        jeonju_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DaySelectActivity.class);
-
-                intent.putExtra("city", jeonju_btn.getText());
-
-                startActivity(intent);
-            }
-        });
-
-        daegu_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DaySelectActivity.class);
-
-                intent.putExtra("city", daegu_btn.getText());
-
-                startActivity(intent);
-            }
-        });
-
-        busan_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DaySelectActivity.class);
-
-                intent.putExtra("city", busan_btn.getText());
-
-                startActivity(intent);
+            public void onClick(View v) {
+                if(tb2.isChecked()){
+                    tb2.setBackgroundDrawable(
+                            getResources().
+                                    getDrawable(R.drawable.s_loc_busan) //위치크기설정필요
+                    );
+                }
+                else{
+                    tb2.setBackgroundDrawable(
+                            getResources().
+                                    getDrawable(R.drawable.loc_busan)
+                    );
+                }
             }
         });
     }
+
 }
+
