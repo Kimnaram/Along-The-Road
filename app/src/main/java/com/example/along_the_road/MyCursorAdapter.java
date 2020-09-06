3f43d1448b3e42754e48208c0a12f40bca057838
YunSongHui <dbsthdgml123@naver.com>
2020-09-06 19:50:02 +0900

    예산관리페이지 뒤로가기 수정 완료, 리스트 삭제 구현 중_20/09/06

--- app/src/main/java/com/example/along_the_road/MyCursorAdapter.java
+++ app/src/main/java/com/example/along_the_road/MyCursorAdapter.java
@@ -1,13 +1,24 @@
 package com.example.along_the_road;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Button;
 import android.widget.CursorAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MyCursorAdapter extends CursorAdapter {
@@ -27,10 +38,12 @@ public class MyCursorAdapter extends CursorAdapter {
 
     //뷰의 속성 지정
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(final View view, final Context context, Cursor cursor) {
 
         TextView item_context = (TextView) view.findViewById( R.id.item_context );
         TextView item_price = (TextView) view.findViewById( R.id.item_price );
+        //ImageButton item_modify = (ImageButton) view.findViewById(R.id.budget_list_modify);
+        final LinearLayout list_area = (LinearLayout)view.findViewById(R.id.list_area);
 
         //getColumnindex(name) : name에 해당하는 필드의 인덱스 번호를 반환한다.
         //cursor.getString(index) : 해당 커서가 위치한 인덱스 위치의 값을 반환한다.
@@ -42,5 +55,35 @@ public class MyCursorAdapter extends CursorAdapter {
         item_context.setText( contexts );
         item_price.setText( price );
 
+        //롱클릭 이벤트
+        list_area.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(final View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage("삭제하시겠습니까?");
+
+                builder.setPositiveButton("예",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(context,"삭제가 왜 안되지..",Toast.LENGTH_LONG).show();
+                            }
+                        });
+
+                //builder.setNegativeButton("아니오",null);
+
+                builder.setNeutralButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        });
+                builder.create().show();
+
+                return false;
+                // true:다른 이벤트 실행 안함, false:다른 이벤트 실행
+            }
+        });
     }
 }
